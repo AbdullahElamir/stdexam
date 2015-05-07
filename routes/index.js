6233b46d276f7d7bbeb7e779449ccc0594ea1943
faroukshouba <faroukshouba@gmail.com>
2015-05-07 12:04:06 +0200

    change template from html to jade

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/reports', function(req, res, next) {
+  res.render('reports', { title: 'reports' });
+});
+
 module.exports = router;
