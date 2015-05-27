f6acbd897aeaaca0a71452432a56f98b86188d55
abdullah Elamir <abdullah.el_ameer@yahoo.com>
2015-05-27 10:23:52 +0200

    app/student

--- app/student.js
+++ app/student.js
@@ -57,6 +57,108 @@
       });
     });
   },
+
+
+   updateName : function(body,cb){
+    mysqlMgr.connect(function (conn) {
+      conn.query('update `student` set name = ? WHERE `idstudent`=?',[body.value,body.pk], function(err, result) {
+        conn.release();
+        if(err) {
+          util.log(err);
+        } else {
+          cb(result);
+        }
+      });
+    });
+  },
+
+   updateMotherName: function(body,cb){
+    mysqlMgr.connect(function (conn) {
+      conn.query('update `student` set  mother_name = ? WHERE `idstudent`=?',[body.value,body.pk], function(err, result) {
+        conn.release();
+        if(err) {
+          util.log(err);
+        } else {
+          cb(result);
+        }
+      });
+    });
+  },
+
+   updateBirthDate : function(body,cb){
+    mysqlMgr.connect(function (conn) {
+      conn.query('update `student` set birth_date = ? WHERE `idstudent`=?',[body.value,body.pk], function(err, result) {
+        conn.release();
+        if(err) {
+          util.log(err);
+        } else {
+          cb(result);
+        }
+      });
+    });
+  },
+
+
+   updatePlaceBirthe : function(body,cb){
+    mysqlMgr.connect(function (conn) {
+      conn.query('update `student` set place_birth= ? WHERE `idstudent`=?',[body.value,body.pk], function(err, result) {
+        conn.release();
+        if(err) {
+          util.log(err);
+        } else {
+          cb(result);
+        }
+      });
+    });
+  },
+
+
+   updateBirthDate : function(body,cb){
+    mysqlMgr.connect(function (conn) {
+      conn.query('update `student` set birth_date = ? WHERE `idstudent`=?',[body.value,body.pk], function(err, result) {
+        conn.release();
+        if(err) {
+          util.log(err);
+        } else {
+          cb(result);
+        }
+      });
+    });
+  },
+
+   updateGender : function(body,cb){
+    mysqlMgr.connect(function (conn) {
+      conn.query('update `student` set gender = ? WHERE `idstudent`=?',[body.value,body.pk], function(err, result) {
+        conn.release();
+        if(err) {
+          util.log(err);
+        } else {
+          cb(result);
+        }
+      });
+    });
+  },
+
+
+  DeleteStudent: function(id,cb){
+     mysqlMgr.connect(function (conn) {
+      conn.query('update `student` set status = 0 WHERE `idstudent`=?',[id,body.pk], function(err, result) {
+        conn.release();
+        if(err) {
+          util.log(err);
+        } else {
+          cb(result);
+        }
+      });
+    });
+  }
+}
+
+
+
+
+
+  
   
 
 
